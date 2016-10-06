cc0fcec709dd373caab99759aa0716bb9cb810a8
David Ray <davduaray@gmail.com>
2016-10-05 23:45:32 -0400

    Add ex01.py

---
+++ ex01.py
@@ -0,0 +1,17 @@
+#print "Hello World!"
+#print "Hello Again."
+#print "I like typing this."
+#print "This is fun."
+#print 'Yay! Printing.'
+#print "I'd much rather you 'not'."
+#print 'I "said" do not touch this.'
+print "Printing an additional line."
+
+# Study Drills
+# 1. Make your script print another line.
+#	 - Added code to Line 8 to print another line.
+# 2. Make your script print only one of the lines.
+#	 - Added # to Lines 1-7. Printing only Line 8.
+# 3. Put a "#" (octothorpe) character at the beginning of a line. What did
+#	 it do? Try to find out what this character does.
+#	 - # tells the interpreter to ignore the line of code
