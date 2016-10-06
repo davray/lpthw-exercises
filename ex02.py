26907701758f60c01ef02b041d6066ebf4693779
David Ray <davduaray@gmail.com>
2016-10-06 02:24:08 -0400

    Add ex02.py

--- ex02.py
+++ ex02.py
@@ -7,3 +7,19 @@ print "I could have code like this." # and the comment after is ignored
 # print "This won't run."
 
 print "This will run."
+
+# Study Drills
+# 1. Find out if you were right about what the # character does and make
+# 	 sure you know what it's called (octothorpe or pound character).
+#	 - I was correct. # tells interpreter to ignore anything after
+# 2. Take ex02.py file and review each line going backward. Start at the
+#	 last line, and check each word in reverse against what you should
+#	 have type.
+#	 - Code reviewed
+# 3. Did you find more mistakes? Fix them.
+#	 - My code was error free.
+# 4. Read what you type previously out loud, including saying each
+#	 character by its name. Did you find more mistakes? Fix them.
+#	 - Code read. Error free. 	 
+
+
