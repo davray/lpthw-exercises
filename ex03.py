b0790d44f7d8cb07e2717a73bede41551bbde973
David Ray <davduaray@gmail.com>
2016-10-06 03:03:50 -0400

    Added Exercise 03 scripts.

---
+++ ex03.py
@@ -0,0 +1,49 @@
+# print string
+print "I will now count my chickens:"
+# print string and product of formula
+print "Hens", 25 + 30 / 6.
+# print string and print product of formula
+print "Roosters", 100 - 25 * 3 % 4.
+# print string
+print "Now I will count the eggs:"
+# print product of formula
+print 3 + 2 + 1 - 5 + 4 % 2 - 1 / 4 + 6.
+# print string
+print "Is it true that 3 + 2 < 5 - 7?"
+# print product of formula
+print 3 + 2 < 5 - 7.
+# print string and sum of formula
+print "What is 3 + 2?", 3 + 2.
+# print string and difference of formula
+print "What is 5 - 7?", 5 - 7.
+# print string
+print "Oh, that's why it's False."
+# print string
+print "How about some more."
+# print string and test inequality
+print "Is it greater?", 5 > -2.
+# print string and test inequality
+print "It is greater or equal?", 5 >= -2.
+# print string and test inequality
+print "Is it less or equal?", 5 <= -2.
+
+# Study Drills
+# 1. Above each line, use the # to write a comment to yourself
+#    explaining what the line does.
+#	 - Addeded personal comments.
+# 2. Remember in exercise 0 when you started Python? Start Python
+#    this way again and, using the above characters and what you know,
+#    use Python as a calculator.
+#	 - Done.
+# 3. Find something you need to calculate and write a new .py file
+#    that does it.
+#	 - Added file ex03_sd3.py
+# 4. Notice the math seems "wrong"? There are no fractions, only whole
+#    numbers. Find out why by researching what a "floating point"
+#    number is.
+#	 - A floating point number is a number that has no fixed number
+#	  before or after the decimal.
+# 5. Rewrite ex03.py to use floating point numbers so it's more
+#    accurate (hint: 20.0 is a floating point).
+#	 - Code eddited to include floating points. Included period after
+#	 formulas
---
+++ ex03_sd3.py
@@ -0,0 +1,13 @@
+# Calculate hours, minutes and seconds
+# Using only what's been learned so far
+
+print "Calculating time.."
+print " "
+
+print "Seconds in a minute: ", 60 * 1
+print "Seconds in a hour: ", 60 * 60
+print "Seconds in a day: ", 60 * 60 * 24
+print "Seconds in a year: ", 60 * 60 * 24 * 365
+print " "
+
+print "Amazing, huh? Imagine all those seconds we take for granted."
