a918c6f9fb1a8bb5d504320db888261b0e347cc0
David Ray <davduaray@gmail.com>
2016-10-07 17:26:25 -0400

    added exercise 07

---
+++ ex07.py
@@ -0,0 +1,40 @@
+# print string
+print "Mary had a little lamb."
+# print string with string format char and value snow
+print "It's fleece was white as %s." % 'snow'
+# print string
+print "And everywhere that Mary went."
+# print string 10 times
+print "." * 10 # what'd that do?
+# assign vars with strings
+end1 = "C"
+end2 = "h"
+end3 = "e"
+end4 = "e"
+end5 = "s"
+end6 = "e"
+end7 = "B"
+end8 = "u"
+end9 = "r"
+end10 = "g"
+end11 = "e"
+end12 = "r"
+
+# watch that comma at the end. try removing it to see what happens
+# print vars
+print end1 + end2 + end3 + end4 + end5 + end6,
+# print vars
+print end7 + end8 + end9 + end10 + end11 + end12
+
+# Study Drill
+# 1. Go back through and write a comment on what each line does.
+#	 - Code commented
+# 2. Read each one backward or out loud to find your errors.
+#	 - Code error free.
+# 3. From now on, when you make mistakes, write down on a piece of
+#    paper what kind of mistake you made.
+# 4. When you go to the next exercise, look at the last mistakes you
+#    made and try not to make them in this new one.
+# 5. Remember that everyone makes mistakes. Programmers are like
+#    magicians who like everyone to think they are perfecr and never
+#    wrong, but it's all an act. They make mistakes all the time.
