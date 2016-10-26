5313d194ede18d3c2833a182a734b29cc94fba5b
David Ray <davduaray@gmail.com>
2016-10-26 16:24:48 -0400

    exercise 34

---
+++ ex34_sd3.py
@@ -0,0 +1,10 @@
+animal = ['bear', 'python', 'peacock', 'kangaroo', 'whale', 'platypus']
+
+print "The animal at 1 is %s." % animal[1]  # cardinal
+print "The 3rd animal is %s." % animal[3-1] # ordinal
+print "The 1st animal is %s." % animal[1-1] # ordinal
+print "The animal at 3 is %s." % animal[3]  # cardinal
+print "The 5th animal is %s." % animal[5-1] # ordinal
+print "The animal at 2 is %s." % animal[2]  # cardinal
+print "The 6th animal is %s." % animal[6-1] # ordinal
+print "The animal at 4 is %s." % animal[4]  # cardinal
